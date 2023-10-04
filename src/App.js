53bab76215e4aeb23ff72238c1a5b49dec7a0190
riadZk <riadzakaria48@gmail.com>
2023-10-04 20:27:55 +0100

    Update File

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Shop } from "./pages/shop/shop"
-import { Contact } from "./pages/contact"
-import { Cart } from "./pages/cart/cart"
-import { ShopContextProvider } from "./context/shop-context"
-import Products from "./components/Product"
-import Signup from "./components/Signup"
-// import { Product } from "./pages/shop/product"
-import Page from "./components/Page"
-import Dashboard from './dashboard/App'
-import Protected from "./components/Protected"
-//  import home from "./pa/Home"
-import Login from "./components/Login"
-import Reports from "./dashboard/page/Reports"
-import Pr from "./dashboard/page/Products"
-import Archives from "./dashboard/page/ArchiveP"
-import Editer from "./dashboard/page/Editer"
-import Message from "./dashboard/page/Message"
-function App() {
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './projet/Home'
+import Sign from './projet/Sign'
+import Tables from './projet/Tables'
+import Contact from './projet/Contact'
+import Admin from './projet/Admin'
+
+function App()  {
+
   return (
-    <div className="App">
-      <ShopContextProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/home" element={<Shop />} />
-            <Route path="/menu" element={<Signup />} />
-            <Route pathe="/page" element={<Page />} />
-            {/* <Route path="/Ajouter" element={<Ajouter />} /> */}
-            <Route path="/dashboard" element={<Protected Component={Dashboard} />} />
-            <Route path="/login" element={<Login />} />
-            <Route path='/reports' element={<Protected Component={Reports} />} />
-            <Route path='/productsA' element={<Protected Component={Pr}/>} />
-            <Route path='/archives' element={<Protected Component={Archives} />} />
-            <Route path='/messages' element={<Protected Component={Message} />} />
-            <Route path="productsA/user/:id/edit" element={<Protected Component={Editer} />} />
-          </Routes>
-        </Router>
-      </ShopContextProvider>
+    <div className=''>
+      <Routes>
+      <Route path="/"  element={<Home />} />
+        <Route path="/Sign" element={<Sign />} />
+        <Route path="/Tables" element={<Tables />} />
+        <Route path="/Admin" element={<Admin />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,69 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   darkMode: "class",
-  content: [ "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'Cdark':"rgb(44, 54, 57)",
-        'Cdark2':"#18122B",
-        'Cdark3':"#181818",
-         'Cdark4':'#0A1931',
-         'Cdark5':'#222831',
-         'Cdark6':'#000000',
-         'Cdark7':'#100F10',
-         'Cdark8':'#1B1B1B',
-         'Cdark9':'#242424',
-         'Linkedin':'#0a66c2',
-         'Twitter':'#1a73e8',
-         'Light':'#EDEDED',
-         'paypal':'#0070ba'
-          
-      }
+      fontFamily: {
+        'dmserif': ['DM Serif', 'serif'],
+        'com': ['Comfortaa', 'cursive'],
+      },
+      animation: {
+        wiggle: 'wiggle 2s ease-in-out 1',
+      },
+      keyframes: {
+        wiggle: {
+          '0%': { 
+            margin: ' 0 0 0 100vh' 
+          },
+          '100%': {
+             margin:
+             '0px' },
+        },
+      },
+      animation: {
+        wiggle2: 'wiggle2 2s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle2: {
+          '50%':{
+            padding:'2.5px 0 0 0 '
+          },
+          '100%': {
+             padding: '0px'
+            },
+        }
+      },
+      colors: {
+        facebook: "#1791f0",
+        twitter: "#1da1f5",
+        maingreen: "#3fac8e",
+        inst:"#020059 ",
+        div:"#091E5F",
+        btn:"#8ab4f8",
+        title:"#70a0ec",
+        navg:" rgb(31, 31, 31)",
+        spacing: {
+          '2.5px': '2.5px',
+        },
+        bg: {
+          DEFAULT:"#f8f9ff",
+          card: "#f1f3fa",
+        },
+        darkbg: {
+          DEFAULT: "#20222f",
+          card: "#252b43",
+          dark: "#1d2029",
+        },
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg: "1024px",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
